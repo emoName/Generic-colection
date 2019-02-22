0499097768442c8791290066b187937a282d3cab
Ion Chiriac <ion.chiriac@amdaris.com>
2019-02-22 17:54:55 +0200

    firtst

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Generics_and_Collections
+{
+    class Class1<T,U>: Class2   where T: class , Interface1, new() where U : struct
+    {
+
+
+        public T ff = default(T);
+        int? e;
+        private U? uurr = new U();
+
+
+        
+            
+            // = ff ?? new T();
+
+        public void method()
+        {
+             U? gg = uurr ?? new U();
+
+            // T where t : class must be not-Nullable
+            //T hh;
+            //T? tt = hh ?? new T();
+
+
+            Console.WriteLine(MyEnum.luni);
+            MyEnum.
+
+        }
+
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Generics_and_Collections
+{
+    enum MyEnum
+    {
+        luni=2,
+        marti,
+            miercuri,
+            joi ,
+            vineri
+
+    }
+}
