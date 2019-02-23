dfea86b618aadfcb617a6481f97b842e756cec35
Ion <chiriac.ion97@outlook.com>
2019-02-23 18:50:17 +0200

    minor  corection

--- Class1.cs
+++ Class1.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace _09_Generics_and_Collections
 {
-    class Class1<T, U> : Class2 where T : class, Interface1//, new()
+    class Class1<T, U>  where T : class, Interface1//, new()
                                 where U : struct
     {
 
